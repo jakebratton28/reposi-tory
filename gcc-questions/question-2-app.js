bea6fb60453892d5ed016bd64d08a8e3fb3cfff9
jakebratton28 <55863972+jakebratton28@users.noreply.github.com>
2019-10-10 22:01:29 -0400

    Create question-2-app.js

---
+++ gcc-questions/question-2-app.js
@@ -0,0 +1,28 @@
+function answer02(risk, bonus, trader) 
+{
+    var total = 0;
+    for (var j = 0; j < trader.length; j++) 
+    {
+        var riskNumbers = [];
+        for (var i = 0; i < risk.length; i++) 
+        {
+            if (trader[j] >= risk[i]) 
+            {
+                riskNumbers.push(i);
+            }
+        }
+        var highest = 0;
+        for (var i = 0; i < riskNumbers.length; i++) 
+        {
+            if (bonus[riskNumbers[i]] > highest) 
+            {
+                highest = bonus[riskNumbers[i]];
+            }
+        }    
+        print(highest);
+        total = total + highest;
+    }
+    return total;
+}
+
+module.exports = answer02
